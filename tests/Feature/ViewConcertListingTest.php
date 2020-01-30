1cc2d4645efcbaba1fd8c39e70d821fe4a97e9b7
benjamin <benjamin@hellomojito.com>
2020-01-30 19:10:50 +0100

    do my first test, miss the class

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -14,11 +14,32 @@ class ViewConcertListingTest extends TestCase
     {
         // Arrange
         // Create a concert
+        $concert = Concert::create([
+           'title' => 'The Red Chord',
+           'subtitle' => 'with Animosity and Letghracy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'PHP',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-555'
+        ]);
 
         // Act
         // View the concert listing
+        $this->visit('/concerts/'.$concert->id);
 
         // Assert
         //  See the concert details
+        $this->see('The Red Chord');
+        $this->see('with Animosity and Letghracy');
+        $this->see('December 13, 2016 8:00pm');
+        $this->see('32.50');
+        $this->see('The Mosh Pit');
+        $this->see('123 Example Lane');
+        $this->see('Laraville, PHP 17916');
+        $this->see('For tickets, call (555) 555-555');
     }
 }
