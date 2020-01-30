df819a60fd45a6e580cd53f5adb6b175ab398f54
benjamin <benjamin@hellomojito.com>
2020-01-30 18:58:46 +0100

    begining with TDD

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -7,5 +7,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ViewConcertListingTest extends TestCase
 {
+    /**
+     * @test
+     */
+    function user_can_view_a_concert_listing()
+    {
+        // Arrange
+        // Create a concert
 
+        // Act
+        // View the concert listing
+
+        // Assert
+        //  See the concert details
+    }
 }
