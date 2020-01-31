8b72fc5defc716aecafc4852512d704d5acae636
benjamin <benjamin@hellomojito.com>
2020-01-31 12:10:08 +0100

    first test created he's complete with Models, Controller

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -2,44 +2,49 @@
 
 namespace Tests\Feature;
 
+use App\Models\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ViewConcertListingTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * @test
      */
-    function user_can_view_a_concert_listing()
+    public function user_can_view_a_concert_listing()
     {
         // Arrange
         // Create a concert
         $concert = Concert::create([
            'title' => 'The Red Chord',
            'subtitle' => 'with Animosity and Letghracy',
-            'date' => Carbon::parse('December 13, 2016 8:00pm'),
-            'ticket_price' => 3250,
-            'venue' => 'The Mosh Pit',
-            'venue_address' => '123 Example Lane',
-            'city' => 'Laraville',
-            'state' => 'PHP',
-            'zip' => '17916',
-            'additional_information' => 'For tickets, call (555) 555-555'
+           'date' => Carbon::parse('December 13, 2016 8:00pm'),
+           'ticket_price' => 3250,
+           'venue' => 'The Mosh Pit',
+           'venue_address' => '123 Example Lane',
+           'city' => 'Laraville',
+           'state' => 'PHP',
+           'zip' => '17916',
+           'additional_information' => 'For tickets, call (555) 555-555'
         ]);
 
         // Act
         // View the concert listing
-        $this->visit('/concerts/'.$concert->id);
+        $response = $this->get('/concerts/'. $concert->id);
 
         // Assert
         //  See the concert details
-        $this->see('The Red Chord');
-        $this->see('with Animosity and Letghracy');
-        $this->see('December 13, 2016 8:00pm');
-        $this->see('32.50');
-        $this->see('The Mosh Pit');
-        $this->see('123 Example Lane');
-        $this->see('Laraville, PHP 17916');
-        $this->see('For tickets, call (555) 555-555');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Letghracy');
+        $response->assertSee('December 13, 2016 8:00pm');
+        $response->assertSee('32.50');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, PHP 17916');
+        $response->assertSee('For tickets, call (555) 555-555');
     }
 }
